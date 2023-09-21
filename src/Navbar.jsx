57cf94e1e6a97938bd0a5a881655c8f686aa567c
krisawyerr <krisawyerrr@gmail.com>
2023-09-20 19:25:42 -0700

    moved pics to public

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,7 +1,7 @@
 export default function Navbar() {
     return (
         <div className="Nav">
-            <img src="/src/assets/icons8-world-50.png" width={40} height={40} className="HeaderImg"/>
+            <img src="/public/assets/icons8-world-50.png" width={40} height={40} className="HeaderImg"/>
             <h1>Kris' Travel Journal.</h1>
         </div>
     )
